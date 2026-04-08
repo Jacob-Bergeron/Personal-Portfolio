5ba6cd61efbc07b7a042c7a46c7985f0ff1eb95d
Jacob Bergeron <jbergeron952@gmail.com>
2026-04-08 12:21:25 -0400

    interactivity to sky image

--- src/components/SkyImage.tsx
+++ src/components/SkyImage.tsx
@@ -1,4 +1,4 @@
-import { Image, Flex } from '@mantine/core';
+import { Image, Flex, Text } from '@mantine/core';
 
 import sky from "../assets/background-sky.jpeg"
 
@@ -6,7 +6,10 @@ const SkyImage = () => {
     return (
 
         <Flex style={{ width: '100%', justifyContent: 'center', paddingTop: '8vh', paddingBottom: '8vh' }} >
-            <Image className='background-image' src={sky} />
+            <div className='sky-image-wrapper'>
+                <Image className='background-image' src={sky} alt='Night sky landscape' />
+                <Text className='sky-image-caption'>Taken in New Hampshire, USA</Text>
+            </div>
         </Flex>
         
     );
